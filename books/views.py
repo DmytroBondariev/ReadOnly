2fe71f805c0c2bceba96aa7c7733769e59e34578
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-28 12:33:27 +0300

    Added book view set with pagination and filtering by title and author

--- books/views.py
+++ books/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from books.models import Book
+from books.serializers import (
+    BookCreateSerializer,
+    BookListSerializer,
+    BookDetailSerializer
+)
+
+
+class BookPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    pagination_class = BookPagination
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BookCreateSerializer
+        elif self.action == "retrieve":
+            return BookDetailSerializer
+        return BookListSerializer
