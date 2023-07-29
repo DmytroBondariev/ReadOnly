a2f7f5849debdbde21c3f86c15750ca17af0261f
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-29 14:25:24 +0300

    Added borrowing view set with list abd retrieve actions; set list action to show all instances only to admin user, non-admin user see only his instances

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingListSerializer, BorrowingDetailSerializer
+
+
+class BorrowingViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = Borrowing.objects.select_related("book", "user")
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return self.queryset
+        return self.queryset.filter(user=user)
+
