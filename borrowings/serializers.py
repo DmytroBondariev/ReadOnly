b286712c4541546c0bcfe4356a0894bba62d28fe
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-29 19:51:47 +0300

    Added  borrowing create serializer with expected return date and inventory validations; added current user adding while to new borrowing instance and book inventory decreasing by 1

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import serializers
 
 from books.serializers import BookDetailBorrowingSerializer
@@ -27,3 +29,33 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("book", "expected_return_date",)
+
+    def validate(self, data):
+        """Check if the book inventory is not 0"""
+        book = data["book"]
+        if book.inventory == 0:
+            raise serializers.ValidationError("Book is not available for borrowing.")
+
+        """Check if the expected_return_date is not in the past"""
+        expected_return_date = data["expected_return_date"]
+        if expected_return_date < date.today():
+            raise serializers.ValidationError("Expected return date must be in the future.")
+
+        return data
+
+    def create(self, validated_data):
+        """Decrease book inventory by 1"""
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+
+        """Attach the current user to the borrowing"""
+        user = self.context["request"].user
+        borrowing = Borrowing.objects.create(user=user, **validated_data)
+        return borrowing
