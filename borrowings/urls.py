c8d3de42d3992026dacec431bce2097afa7313f8
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-29 14:40:00 +0300

    Added corresponding endpoints

---
+++ borrowings/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from borrowings.views import BorrowingViewSet
+
+borrowing_list = BorrowingViewSet.as_view(actions={"get": "list"})
+borrowing_detail = BorrowingViewSet.as_view(actions={"get": "retrieve"})
+
+urlpatterns = [
+    path("borrowings/", borrowing_list, name="borrowing-list"),
+    path("borrowings/<int:pk>", borrowing_detail, name="borrowing-detail"),
+]
+
+
+app_name = "borrowings"
