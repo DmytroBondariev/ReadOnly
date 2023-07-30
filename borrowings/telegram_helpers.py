b245655240904e2aac9f97b95c8089ae965931e0
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-30 19:30:24 +0300

    Changed borrowing notification function to async

--- borrowings/telegram_helpers.py
+++ borrowings/telegram_helpers.py
@@ -1,10 +1,20 @@
-import os
-from telegram import Bot
+import asyncio
+from aiogram import Bot
 
 
-def send_telegram_notification(message):
-    bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
-    chat_id = os.environ.get("TELEGRAM_CHAT_ID")
+async def send_telegram_notification(message):
+    bot_token = "6155357404:AAG_oKggBJTFzJsOPokvQ1SwU5eqWMh34gk"
+    chat_id = 382864435
 
     bot = Bot(token=bot_token)
-    bot.send_message(chat_id=chat_id, text=message)
+    await bot.send_message(chat_id=chat_id, text=message)
+
+
+async def main():
+    message = "Hello, this is a test notification!"
+    await send_telegram_notification(message)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
