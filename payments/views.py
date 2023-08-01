723049d0b77a816aa24c7183e7fac1374aa6a2d7
Dmytry Bondariev <bondarievmd@gmail.com>
2023-08-01 23:58:38 +0300

    Added list retrieve viewset with current user queryset

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import PaymentListSerializer, PaymentDetailSerializer
+
+
+class PaymentViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = Payment.objects.select_related("borrowing")
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        user = self.request.user
+
+        if user.is_staff:
+            return queryset
+        return queryset.filter(borrowing__user=user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
