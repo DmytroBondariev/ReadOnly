6d3029eb3a1f02c84fa0b66be68fbd71515cc71d
Dmytry Bondariev <bondarievmd@gmail.com>
2023-08-04 12:44:21 +0300

    Added payment success custom action; added corresponding endpoint

--- payments/urls.py
+++ payments/urls.py
@@ -4,9 +4,12 @@ from payments.views import PaymentViewSet
 
 payment_list = PaymentViewSet.as_view(actions={"get": "list"})
 payment_detail = PaymentViewSet.as_view(actions={"get": "retrieve"})
+payment_success = PaymentViewSet.as_view(actions={"get": "success"})
+
 urlpatterns = [
     path("payments/", payment_list, name="payment-list"),
     path("payments/<int:pk>", payment_detail, name="payment-detail"),
+    path("payments/success/", payment_success, name="payment-success"),
 ]
 
 
