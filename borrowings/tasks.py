fb02220ae986785bfa889087d24eb1be82c374e1
Dmytry Bondariev <bondarievmd@gmail.com>
2023-08-29 01:02:37 +0300

    Rolled back changes in tasks and borrowings views

--- borrowings/tasks.py
+++ borrowings/tasks.py
@@ -1,19 +1,32 @@
 import asyncio
+
 from celery import shared_task
 from aiogram import Bot
 import os
+import logging
+
 from django.utils import timezone
 from dotenv import load_dotenv
 
 from borrowings.models import Borrowing
-from .telegram_helpers import send_telegram_notification
 
+logger = logging.getLogger(__name__)
 load_dotenv()
 
 
 @shared_task
 def send_telegram_notification(message):
-    send_telegram_notification.delay(message)
+    bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
+    chat_id = os.environ.get("TELEGRAM_CHAT_ID")
+
+    bot = Bot(token=bot_token)
+
+    async def send_notification():
+        await bot.send_message(chat_id=chat_id, text=message)
+
+    """Running the async function in the event loop"""
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_notification())
 
 
 @shared_task
@@ -36,4 +49,3 @@ def check_overdue_borrowings():
                 f"Expected return date: {borrowing.expected_return_date}."
             )
             send_telegram_notification.delay(message)
-
